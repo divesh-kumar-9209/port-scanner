f475a74890be61337f4ac86fdc63ffe692ed4bdd
Divesh Kumar <diveshkumar9209@outlook.com>
2026-03-11 19:13:14 +0530

    Version 6

--- port_scanner.py
+++ port_scanner.py
@@ -1,27 +1,50 @@
 import socket
 import time
 import threading
+import argparse
 
-target = input("Enter target IP address: ")
-start_port = int(input("Enter start port: "))
-end_port = int(input("Enter end port: "))
+# Terminal colors
+GREEN = "\033[92m"
+CYAN = "\033[96m"
+YELLOW = "\033[93m"
+RESET = "\033[0m"
+
+print(f"""
+{CYAN}
+========================================
+        BYTESBREACH PORT SCANNER
+========================================
+{RESET}
+""")
+
+parser = argparse.ArgumentParser(description="Multithreaded Port Scanner")
+
+parser.add_argument("target", help="Target IP or hostname")
+parser.add_argument("-p", "--ports", help="Port range (example: 20-200)", required=True)
+
+args = parser.parse_args()
+
+target = args.target
+port_range = args.ports.split("-")
+
+start_port = int(port_range[0])
+end_port = int(port_range[1])
 
 try:
     target = socket.gethostbyname(target)
 except socket.gaierror:
-    print("Invalid hostname.")
+    print("Invalid hostname")
     exit()
 
-print("\n---------------------------------")
-print("Target:", target)
-print("Scanning ports", start_port, "to", end_port)
-print("---------------------------------\n")
+print(f"{YELLOW}Target      : {target}{RESET}")
+print(f"{YELLOW}Port Range  : {start_port} - {end_port}{RESET}\n")
 
 start_time = time.time()
 
 open_ports = []
 
 def scan_port(port):
+
     scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     scanner.settimeout(1)
 
@@ -29,7 +52,6 @@ def scan_port(port):
         result = scanner.connect_ex((target, port))
 
         if result == 0:
-            banner = ""
 
             try:
                 scanner.send(b"Hello\r\n")
@@ -37,15 +59,17 @@ def scan_port(port):
             except:
                 banner = "Service detected"
 
-            print(f"[OPEN] Port {port} → {banner}")
+            print(f"{GREEN}[OPEN]{RESET} {port:<5} → {banner}")
             open_ports.append(port)
 
     finally:
         scanner.close()
 
+
 threads = []
 
 for port in range(start_port, end_port + 1):
+
     thread = threading.Thread(target=scan_port, args=(port,))
     threads.append(thread)
     thread.start()
@@ -55,8 +79,8 @@ for thread in threads:
 
 end_time = time.time()
 
-print("\n---------------------------------")
-print("Scan completed.")
+print("\n----------------------------------------")
+print(f"{CYAN}Scan completed{RESET}")
 print("Open ports:", sorted(open_ports))
-print("Total scan time:", round(end_time - start_time, 2), "seconds")
-print("---------------------------------")
+print("Scan time :", round(end_time - start_time, 2), "seconds")
+print("----------------------------------------")
