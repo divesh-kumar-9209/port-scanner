5d8e26ed976da48573fc8e8b73b91232abc3f7a7
Divesh Kumar <diveshkumar9209@outlook.com>
2026-03-11 18:01:51 +0530

    Version 2

--- port_scanner.py
+++ port_scanner.py
@@ -1,11 +1,15 @@
 import socket
 
 target = input("Enter target IP address: ")
+start_port = int(input("Enter start port: "))
+end_port = int(input("Enter end port: "))
 
-print("\nScanning target:", target)
-print("Scanning ports 1 to 1024...\n")
+print("\n---------------------------------")
+print("Target:", target)
+print("Scanning ports", start_port, "to", end_port)
+print("---------------------------------\n")
 
-for port in range(1, 1025):
+for port in range(start_port, end_port + 1):
 
     scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     scanner.settimeout(1)
@@ -13,7 +17,7 @@ for port in range(1, 1025):
     result = scanner.connect_ex((target, port))
 
     if result == 0:
-        print("Port", port, "is OPEN")
+        print("[OPEN] Port", port)
 
     scanner.close()
 
