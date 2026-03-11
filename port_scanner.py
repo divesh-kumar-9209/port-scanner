976f905cdb97a2c0d45ae398113fa638b595ec10
Divesh Kumar <diveshkumar9209@outlook.com>
2026-03-11 18:09:43 +0530

    Version 3

--- port_scanner.py
+++ port_scanner.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 target = input("Enter target IP address: ")
 start_port = int(input("Enter start port: "))
@@ -9,6 +10,10 @@ print("Target:", target)
 print("Scanning ports", start_port, "to", end_port)
 print("---------------------------------\n")
 
+start_time = time.time()
+
+open_ports = []
+
 for port in range(start_port, end_port + 1):
 
     scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,8 +22,15 @@ for port in range(start_port, end_port + 1):
     result = scanner.connect_ex((target, port))
 
     if result == 0:
+        open_ports.append(port)
         print("[OPEN] Port", port)
 
     scanner.close()
 
-print("\nScan completed.")
+end_time = time.time()
+
+print("\n---------------------------------")
+print("Scan completed.")
+print("Open ports:", open_ports)
+print("Total scan time:", round(end_time - start_time, 2), "seconds")
+print("---------------------------------")
